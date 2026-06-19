87cf6ce87d15405a8330def8fbf182ec552fcd70
Daniel Danis <daniel.danis@cse-cst.gc.ca>
2026-06-19 17:59:58 +0000

    use case 1

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "api_key" {
     description = "API key"
     type = string
 }
+
+variable "syslog_host_ip" {
+    description = "IP address of the host running syslog container"
+    type        = string
+}
